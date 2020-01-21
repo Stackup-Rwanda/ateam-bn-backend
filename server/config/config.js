d0480710ddf616616e9ceb3fcccf10e850b7d474
Doddy <kwizeradoddy@gmail.com>
2020-01-21 13:34:25 +0200

    ch(setup): setup sequelize and migrations [Finishes #170766071]

---
+++ server/config/config.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const development = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DEVELOP_DB_NAME,
+  host: process.env.HOST,
+  dialect: "postgres",
+};
+
+const test = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.TEST_DB_NAME,
+  host: process.env.HOST,
+  dialect: "postgres",
+};
+
+const production = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.PRODUCTION_DB_NAME,
+  host: process.env.HOST,
+  dialect: "postgres",
+};
+
+export {
+  development,
+  test,
+  production
+};
