21d929b8b0bf8d84c2718eea7dbb85668feae7b0
key_joshua <k.joshua855@gmail.com>
2020-01-21 14:31:02 +0200

    Done with configuring swagger documentation
    ch(done-with-configuring-swagger-documentation):As owner of this i have configured swagger documentation for application[finishes #170766078]

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import apiDocumentation from '../swagger.json';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const basePath = '/api/v-unknown';
+
+app.listen(process.env.PORT || 3000, () => { console.log('server is running on port 3000'); });
+app.use(`${basePath}/documentation`, swaggerUi.serve, swaggerUi.setup(apiDocumentation));
+app.get('**', (req, res) => {
+    res.status(400).send({ 
+        status: 400,
+        message: 'Hey !! You are Welcome to BareFoot Nomad, Use the link below it is documentation of application',
+        data: docUrl,
+    });
+  });
+
+export default app;
