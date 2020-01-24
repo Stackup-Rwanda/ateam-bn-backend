abe8a96486127bab33f5574fa972155a883620fb
key_joshua <k.joshua855@gmail.com>
2020-01-24 04:01:18 +0200

    resolved conflict

--- server/index.js
+++ server/index.js
@@ -7,13 +7,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const basePath = '/api/v-unknown';
 
-app.listen(process.env.PORT || 3000, () => { console.log('server is running on port 3000'); });
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server is running on port 3000');
+});
 app.use(`${basePath}/documentation`, swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 app.get('**', (req, res) => {
   res.status(400).send({
     status: 400,
-    message: 'Hey !! are you looking for BareFoot Nomad,  If yes Use the link below it is documentation of application',
-    data: '/api/v-unknown/documentation/',
+    message: `Hey !! You are Welcome to BareFoot Nomad, Use the link below its documentation of application`,
+    data: `/api/v-unknown/documentation`
   });
 });
 
