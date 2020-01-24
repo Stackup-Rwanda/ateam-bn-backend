d3983f5661b2585f3b726e6ec6453a56a0f478df
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2020-01-24 14:15:38 +0200

    ch(set-first-test): hound debug 2 [Finishes #170766078]

--- server/tests/index.spec.js
+++ server/tests/index.spec.js
@@ -6,7 +6,7 @@ Chai.should();
 Chai.use(chaiHttp);
 
 describe('Endpoint /', () => {
-  it("should welcome a users and redirect them to the documentation", done => {
+  it("should welcome a users and redirect them to the documentation", (done) => {
     Chai.request(app).get("/anywrongurls").end((err, res) => {
       res.should.have.status(400);
       res.body.should.have.property("message", "Hey !! You are Welcome to BareFoot Nomad, Use the link below its documentation of application");
