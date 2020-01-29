4f7dd98fabf05ea8cd8f57949d853d116e29b7f2
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2020-01-30 01:27:45 +0200

    ft(signup-endpoint): fix hound bugs [Finishes #170766030]

--- migrations/20200129004409-alterUserAttributes.js
+++ migrations/20200129004409-alterUserAttributes.js
@@ -1,63 +1,60 @@
-const up = (queryInterface, Sequelize) => {
-  return queryInterface.createTable('users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    name: {
-      type: Sequelize.STRING
-    },
-    gender: {
-      type: Sequelize.STRING
-    },
-    email: {
-      type: Sequelize.STRING,
-      unique : true
-    },
-    username: {
-      type: Sequelize.STRING,
-      unique : true
-    },
-    password: {
-      type: Sequelize.STRING
-    },
-    birthdate: {
-      type: Sequelize.DATE
-    },
-    preferredLanguage: {
-      type: Sequelize.STRING
-    },
-    preferredCurrency: {
-      type: Sequelize.STRING
-    },
-    location: {
-      type: Sequelize.STRING
-    },
-    role: {
-      type: Sequelize.STRING
-    },
-    department: {
-      type: Sequelize.STRING
-    },
-    lineManager: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+const up = (queryInterface, Sequelize) => queryInterface
+.createTable('users', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  gender: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  username: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  birthdate: {
+    type: Sequelize.DATE
+  },
+  preferredLanguage: {
+    type: Sequelize.STRING
+  },
+  preferredCurrency: {
+    type: Sequelize.STRING
+  },
+  location: {
+    type: Sequelize.STRING
+  },
+  role: {
+    type: Sequelize.STRING
+  },
+  department: {
+    type: Sequelize.STRING
+  },
+  lineManager: {
+    type: Sequelize.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE
+  }
+});
 
-const down = (queryInterface, Sequelize) => {
-  return queryInterface.dropTable('users');
-};
+const down = (queryInterface) => queryInterface.dropTable('users');
 
 export {
   up,
