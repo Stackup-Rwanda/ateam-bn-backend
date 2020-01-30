c6b282270c242082ff1fe450c0b3fde7add38d91
blaise82 <nigorjeanluc@gmail.com>
2020-01-30 18:30:46 +0200

    ft(signup-endpoint): edit swagger doc for this endpoint [Finishes #170766030]

--- server/tests/authTest.js
+++ server/tests/authTest.js
@@ -73,11 +73,10 @@ describe('Test for signup endpoint', () => {
       expect(res.body.data).to.be.an('object');
     })
   );
+
 // after(async () => {
 //   await sequelize.close();
 // });
-
-<<<<<<< HEAD
   it(
     "shouldn't signup already saved user",
     mochaAsync(async () => {
@@ -113,30 +112,29 @@ describe('signIn tests', () => {
   });
 });
 
-// describe('Test for signup endpoint', () => {
-//   it(
-//     'should create a new user account with appropriate request',
-//     mochaAsync(async () => {
-//       const res = await router()
-//         .post('/api/auth/signup')
-//         .send(usersTester[0]);
-//       expect(res.body.status).to.equal(201);
-//       expect(res.body).to.be.an('object');
-//       expect(res.body.message).to.be.a('string');
-//       expect(res.body.data).to.be.an('object');
-//     })
-//   );
+describe('Test for signup endpoint', () => {
+  it(
+    'should create a new user account with appropriate request',
+    mochaAsync(async () => {
+      const res = await router()
+        .post('/api/auth/signup')
+        .send(usersTester[0]);
+      expect(res.body.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body.message).to.be.a('string');
+      expect(res.body.data).to.be.an('object');
+    })
+  );
 
-//   it(
-//     "shouldn't signup already saved user",
-//     mochaAsync(async () => {
-//       const res = await router()
-//         .post('/api/auth/signup')
-//         .send(usersTester[1]);
-//       expect(res.body.status).to.equal(409);
-//       expect(res.body).to.be.an('object');
-//       expect(res.body.error).to.be.a('string');
-//     })
-//   );
-// });
-
+  it(
+    "shouldn't signup already saved user",
+    mochaAsync(async () => {
+      const res = await router()
+        .post('/api/auth/signup')
+        .send(usersTester[1]);
+      expect(res.body.status).to.equal(409);
+      expect(res.body).to.be.an('object');
+      expect(res.body.error).to.be.a('string');
+    })
+  );
+});
