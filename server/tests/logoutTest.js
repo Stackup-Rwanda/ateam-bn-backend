93a8dc1fd9f8b4ed74e1e2b8da0e7b9af759c572
bihire <habimanaolivier6@gmail.com>
2020-01-31 09:46:08 +0200

    raised my tests to the tests expected on the project

--- server/tests/logoutTest.js
+++ server/tests/logoutTest.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 const router = () => chai.request(app);
 describe('my Testing suite', () => {
   const validtoken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiY2F0ZWdvcnkiOiJhZG1pbiIsImZpcnN0bmFtZSI6Im1pbmFuaSIsImxhc3RuYW1lIjoiam9zaHVhIiwidXNlcm5hbWUiOiJrZXktam9zaHVhIiwiZW1haWwiOiJrLmpvc2h1YTg1NUBnbWFpbC5jb20iLCJwaG9uZW51bWJlciI6IjA3ODk2MTk0NDIiLCJwYXNzd29yZCI6IiQyYSQxMiQzVDN3RG1QZU5CYVplOXVGZm0yM3plZ3VVT29Sd1d6cDVuMUU3LnJUWnNWcVp1aXBLN1NvbSIsImNyZWF0ZWRkYXRlIjoiMjAxOS0xMS0wNFQxNjowMjoyMi4zMTVaIiwibW9kaWZpZWRkYXRlIjoiIG5vbmUiLCJpYXQiOjE1ODAyNzg5MjgsImV4cCI6MTY2NjY3ODkyOH0.GBTZe8FjA7dvw3NWlPfXVQxBEHFlap6798A71C61fs0`;
-  it('users should be able to logout from application', done => {
+  it('users should be able to logout from application', (done) => {
     router()
       .get('/api/v-unknown/users/logout')
       .set('Authorization', validtoken)
