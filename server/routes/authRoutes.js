cfa10099a5e8b230abf241f65f66a7c0fc72078e
Ishoborabyose <habimanaolivier6@gmail.com>
2020-01-31 16:21:00 +0200

    ft-login-via-facebook-and-google-170766085(facebook login):

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,32 +1,31 @@
 import Router from 'express';
-
+import passport from 'passport';
+import googleAuth from '../controllers/googleAuthController';
 import AuthController from '../controllers/authController';
 import asyncErrorHandler from '../helpers/asyncErrorHandler';
-<<<<<<< HEAD
-import passwordHasher from '../middlewares/passwordHashMiddleware';
-=======
 import storeAuth from '../controllers/fbAuthController';
->>>>>>> ft-login-via-facebook-and-google-170766085(facebook and google login test): test social logins
 
 const router = Router();
-
+router.use(passport.initialize());
 router.post(
   '/auth/signup',
-  passwordHasher,
   asyncErrorHandler(AuthController.signUp)
 );
 
-<<<<<<< HEAD
-=======
-router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
-router.get('/auth/google/callback',
-  passport.authenticate('google', { session: false }), googleAuth);
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { session: false }), googleAuth
+);
 
 
-router.get('/auth/facebook',
-  passport.authenticate('facebook'));
+router.get(
+  '/auth/facebook',
+  passport.authenticate('facebook')
+);
 router.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), storeAuth);
 
->>>>>>> ft-login-via-facebook-and-google-170766085(facebook and google login test): test social logins
 export default router;
