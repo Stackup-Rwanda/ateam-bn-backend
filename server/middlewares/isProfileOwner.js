75a5e5f61744697fc502a2c7b0dbd7676f239aae
Doddy <kwizeradoddy@gmail.com>
2020-01-31 13:39:50 +0200

    ft(api): add view profile endpoint and edit profile endpoint [Finishes #170766089]

---
+++ server/middlewares/isProfileOwner.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import models from '../models';
+
+const { User } = models;
+dotenv.config();
+
+const ownerVerifier = async (req, res, next) => {
+  try {
+    const verified = jwt.verify(req.header('token'), process.env.JWT_PRIVATE_KEY);
+    req.requesterEmail = verified.email;
+    const pretender = await User.findOne({
+      where: { email: req.requesterEmail }
+    });
+    if (pretender) {
+      if (pretender.username === verified.username && pretender.name === verified.name
+        && req.params.username
+        === pretender.username) {
+        req.requesterId = pretender.id;
+        return next();
+      }
+      res.status(401).json({ status: 401, error: 'unauthorized, profile not owned or token bears wrong data' });
+    }
+    res.status(401).json({ status: 401, error: 'User not recognised' });
+  } catch (error) {
+    res.status(400).json({ status: 400, error: 'Malformed security token, check token and try again' });
+  }
+};
+
+export default ownerVerifier;
