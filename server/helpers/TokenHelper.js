768ab14a3c8c2f0f43370b4c21ec295aa62a167c
blaise82 <nigorjeanluc@gmail.com>
2020-01-31 13:22:24 +0200

    ft-(generate-jwt): create helper to generate and decode token [Finishes #170766081]

---
+++ server/helpers/TokenHelper.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+/**
+ * This class contains
+ * two methods, one to help hashing password (hashPassword)
+ * and the second to retrieve hashed password
+ */
+class TokenHelper {
+  /**
+   * Hashs the password.
+   * @param {string} password The user's password.
+   * @returns {string} The users's hashed password.
+   */
+  static generateResetPasswordToken({
+    id, email, password, createdAt,
+  }) {
+    const secrect = `${password}_${createdAt}`;
+    return jwt.sign({ id, email }, secrect, { expiresIn: 3600 });
+  }
+  /**
+   * Hashs the password.
+   * @param {string} token The user's token.
+   * @param {string} secrectKey The secret key.
+   * @returns {string} The users's hashed password.
+   */
+  static decodedToken(token, secrectKey) {
+    return jwt.verify(token, secrectKey);
+  }
+}
+export default TokenHelper;
