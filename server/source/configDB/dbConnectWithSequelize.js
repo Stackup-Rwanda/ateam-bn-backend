970e35d6497019b394484c176a6960f228777469
bihire <habimanaolivier6@gmail.com>
2020-01-31 02:57:29 +0200

    fixing Error: Dialect needs to be explicitly supplied as of v4.0.0

--- server/source/configDB/dbConnectWithSequelize.js
+++ server/source/configDB/dbConnectWithSequelize.js
@@ -2,5 +2,5 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const databaseConnection = new Sequelize(process.env.databaseURL);
+const databaseConnection = new Sequelize(process.env.databaseURL, { dialect: 'postgres' });
 export default databaseConnection;
