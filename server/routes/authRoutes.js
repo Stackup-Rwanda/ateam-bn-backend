2a812802dde30553a8d7df1defaeae7ddefb31af
Ishoborabyose <habimanaolivier6@gmail.com>
2020-02-01 17:20:47 +0200

    ft-login-via-facebook-and-google-170766085(facebook and google login test): test social logins [continue #170766085]

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,11 @@ import Router from 'express';
 
 import AuthController from '../controllers/authController';
 import asyncErrorHandler from '../helpers/asyncErrorHandler';
+<<<<<<< HEAD
 import passwordHasher from '../middlewares/passwordHashMiddleware';
+=======
+import storeAuth from '../controllers/fbAuthController';
+>>>>>>> ft-login-via-facebook-and-google-170766085(facebook and google login test): test social logins
 
 const router = Router();
 
@@ -12,4 +16,17 @@ router.post(
   asyncErrorHandler(AuthController.signUp)
 );
 
+<<<<<<< HEAD
+=======
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+router.get('/auth/google/callback',
+  passport.authenticate('google', { session: false }), googleAuth);
+
+
+router.get('/auth/facebook',
+  passport.authenticate('facebook'));
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), storeAuth);
+
+>>>>>>> ft-login-via-facebook-and-google-170766085(facebook and google login test): test social logins
 export default router;
