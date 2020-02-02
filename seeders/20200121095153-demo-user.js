f460a90c42b41f382dc928894bdbea9ba9b2c83d
IGIHOZO Jean Christian <igihozo35@gmail.com>
2020-02-02 13:26:49 +0200

    bug fixing refactoring

--- seeders/20200121095153-demo-user.js
+++ seeders/20200121095153-demo-user.js
@@ -3,7 +3,7 @@ import Hasher from '../server/helpers/passwordHashHelper';
 const up = (queryInterface) => queryInterface.bulkInsert('Users', [{
   name: 'Dummy',
   gender: 'Male',
-  email: 'dummy1@email.rw',
+  email: 'dummy@email.rw',
   username: 'MrDummy',
   password: Hasher.hashPassword('123456789'),
   birthdate: new Date(),
--- server/routes/allRoutes.js
+++ server/routes/allRoutes.js
@@ -1,11 +1,9 @@
 import Router from 'express';
 
-import authRoutes from './authRoutes';
 import tripRoutes from './tripRoutes';
 
 const router = Router();
 
-router.use(authRoutes);
 router.use(tripRoutes);
 
 export default router;
