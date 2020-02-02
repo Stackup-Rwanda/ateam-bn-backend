81976b3b0afe670aad63d857f0c1f242db5c23f4
jajajaden01 <jajajaden01@gmail.com>
2020-02-02 15:01:03 +0200

    setting up ft-create-or-delete-comment-on-travel-request-170766098 branch

--- server/source/configDB/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const pool = new pg.Pool({ connectionString: process.env.databaseURL });
-if (pool) console.log("connected successfully to barefoot Database");
-
-
-export default pool;
