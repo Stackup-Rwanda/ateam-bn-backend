e910f9c29bdc9ec14a70f5337db8df8e97d40570
Ishoborabyose <habimanaolivier6@gmail.com>
2020-02-02 18:04:16 +0200

    ft(social login test): user logins via facebook and google [Finishes #170766085]

--- server/strategies/fbStrategy.js
+++ server/strategies/fbStrategy.js
@@ -3,32 +3,32 @@ import passport from 'passport';
 import dotenv from 'dotenv';
 import User from '../models/user';
 import db from '../config/db';
+
 dotenv.config();
 
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  callbackURL: process.env.callbackURL
+},
+async (accessToken, refreshToken, profile, cb) => {
+  console.log(profile);
+  cb(null, profile);
 
+  const { id, displayName } = profile;
+  await User.findAll({
+    limit: 1,
+    where: { fb_id: id }
+  });
+  if (!User.length) {
+    await db.sync().then(() => {
+      User.create({
+        name: profile.displayName,
+        fb_id: profile.id
 
-   passport.use(new FacebookStrategy({
-    clientID: process.env.clientID,
-     clientSecret: process.env.clientSecret,
-     callbackURL: process.env.callbackURL
-   },
-   async(accessToken, refreshToken, profile,cb)=> {
-    console.log(profile);
-   cb(null, profile);
-
-   const {id,displayName}= profile;
-    await User.findAll({
-      limit:1,
-        where: {fb_id:id }
       });
-      if(!User.length){
-        await db.sync().then(()=>{ User.create({
-            name: profile.displayName,
-            fb_id: profile.id
- 
-});})
-   console.log(id);     
-}})
-
-);
+    });
+    console.log(id);
+  }
+}));
