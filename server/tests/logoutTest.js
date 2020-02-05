453c4fffe0f99e1c477dbf8c1f3bea21f1b6e2dd
bihire <habimanaolivier6@gmail.com>
2020-02-05 12:02:04 +0200

    Added validtoken function

--- server/tests/logoutTest.js
+++ server/tests/logoutTest.js
@@ -5,11 +5,11 @@ import app from '../index';
 chai.use(chaiHttp);
 const router = () => chai.request(app);
 describe('my Testing suite', () => {
-  const validtoken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiY2F0ZWdvcnkiOiJhZG1pbiIsImZpcnN0bmFtZSI6Im1pbmFuaSIsImxhc3RuYW1lIjoiam9zaHVhIiwidXNlcm5hbWUiOiJrZXktam9zaHVhIiwiZW1haWwiOiJrLmpvc2h1YTg1NUBnbWFpbC5jb20iLCJwaG9uZW51bWJlciI6IjA3ODk2MTk0NDIiLCJwYXNzd29yZCI6IiQyYSQxMiQzVDN3RG1QZU5CYVplOXVGZm0yM3plZ3VVT29Sd1d6cDVuMUU3LnJUWnNWcVp1aXBLN1NvbSIsImNyZWF0ZWRkYXRlIjoiMjAxOS0xMS0wNFQxNjowMjoyMi4zMTVaIiwibW9kaWZpZWRkYXRlIjoiIG5vbmUiLCJpYXQiOjE1ODAyNzg5MjgsImV4cCI6MTY2NjY3ODkyOH0.GBTZe8FjA7dvw3NWlPfXVQxBEHFlap6798A71C61fs0`;
+  const validtoken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQnV0aXJpZ2l0d2EgTWFuemkiLCJ1c2VybmFtZSI6Im1hbnppIiwiZW1haWwiOiJtYW56aUBnbWFpbC5jb20iLCJpYXQiOjE1ODA4MjA1MTZ9.jPcc3YOJCGZaq-3Y-hQNQG_VlzijnocglCg5b0twHYA`;
   it('users should be able to logout from application', (done) => {
     router()
       .get('/api/users/logout')
-      .set('Authorization', validtoken)
+      .set('token', validtoken)
       .end((error, response) => {
         expect(response).to.have.status([200]);
         expect(response.body).to.be.a('object');
