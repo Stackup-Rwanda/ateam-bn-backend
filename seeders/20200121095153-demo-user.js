6c8af159070fa6a97020aec42fc1fd772c37969d
bihire <habimanaolivier6@gmail.com>
2020-02-05 12:22:06 +0200

    added seed data

---
+++ seeders/20200121095153-demo-user.js
@@ -0,0 +1,43 @@
+import Hasher from '../server/helpers/passwordHashHelper';
+
+const up = (queryInterface) => queryInterface.bulkInsert('Users', [{
+  name: 'Dummy',
+  gender: 'Male',
+  email: 'dummy@email.rw',
+  username: 'MrDummy',
+  password: Hasher.hashPassword('123456789'),
+  birthdate: new Date(),
+  preferredLanguage: 'French',
+  preferredCurrency: 'Euro',
+  location: 'Paris',
+  role: 'Broker',
+  department: 'Cleaner',
+  lineManager: 'MrNobody',
+  isVerified: false,
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: 'Butirigitwa Manzi',
+  gender: 'Male',
+  email: 'manzi@gmail.com',
+  username: 'manzi',
+  password: Hasher.hashPassword('123456789'),
+  birthdate: new Date(),
+  preferredLanguage: 'French',
+  preferredCurrency: 'Euro',
+  location: 'Paris',
+  role: 'Broker',
+  department: 'Cleaner',
+  lineManager: 'MrNobody',
+  isVerified: false,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}]);
+
+const down = (queryInterface) => queryInterface.bulkDelete('Users', null, {});
+
+export {
+  up,
+  down
+};
