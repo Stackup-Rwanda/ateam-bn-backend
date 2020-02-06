4223d218dc4d0611e463d4ae974763acc31623fa
Doddy <kwizeradoddy@gmail.com>
2020-02-06 03:25:30 +0200

    ft(app settings): add role assignment functionality [Finishes #170766095]

--- server/routes/allRoutes.js
+++ server/routes/allRoutes.js
@@ -2,10 +2,12 @@ import Router from 'express';
 import profileRoute from './userProfile';
 
 import authRoutes from './authRoutes';
+import roleRoutes from './userRoleRoutes';
 
 const router = Router();
 
 router.use(authRoutes);
 router.use(profileRoute);
+router.use(roleRoutes);
 
 export default router;
