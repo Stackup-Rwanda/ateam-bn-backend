8f217c815bed9fbdb10a4b9f19822000e2d46074
jajajaden01 <jajajaden01@gmail.com>
2020-02-10 15:35:35 +0200

    implementing feedback

--- server/controllers/EmailController.js
+++ server/controllers/EmailController.js
@@ -76,7 +76,7 @@ class EmailController {
     await AuthHelpers.updateUserPassword(userExist.id, req.body);
     const subjectAndHhtmlBody = passwordResetWellSubjectAndHtmlBoy(userExist);
     const theMessage = sendEmailTemplate('support@borafoot.com', userExist, subjectAndHhtmlBody);
-    sgMail.send(theMessage);
+    await sgMail.send(theMessage);
 
     return res.status(200).json({
       status: res.statusCode,
