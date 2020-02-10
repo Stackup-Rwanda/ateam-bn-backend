a7b488eec3c39d08d3065ad997a0601f0d26f3aa
jajajaden01 <jajajaden01@gmail.com>
2020-02-10 11:03:07 +0200

    solving the sprint 1 feedback

--- server/controllers/socialController.js
+++ server/controllers/socialController.js
@@ -1,20 +1,19 @@
 import AuthHelper from '../helpers/authHelpers';
 import responses from '../helpers/responsesHelper';
-import authorization from '../helpers/socialResponderHelper';
+import authorize from '../helpers/socialResponderHelper';
 
-const storeAuth = (profile, fb) => {
-  AuthHelper.userExists('email', profile.user.emails[0].value).then((facebookExists) => {
-    authorization(facebookExists, fb);
-    AuthHelper.saveSocial(profile.user).then((user) => responses(201, fb, user));
+const storeAuth = (profile, fbResponse) => {
+  AuthHelper.userExists('email', profile.user.emails[0].value).then((facebookUser) => {
+    authorize(facebookUser, fbResponse);
+    AuthHelper.saveSocial(profile.user).then((user) => responses(201, fbResponse, user));
   });
 };
 const googleAuth = async (req, res) => {
   const doesExists = await AuthHelper.userExists('email', req.user.emails[0].value);
-  authorization(doesExists, res);
+  authorize(doesExists, res);
   const user = await AuthHelper.saveSocial(req.user);
   responses(201, res, user);
 };
-
 export
 {
   storeAuth,
